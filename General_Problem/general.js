9f691dd76272c7b29699bb4cf69baccae5463dbe
samir7171 <avhadsamir66@gmail.com>
2023-11-05 21:48:14 +0530

    added new js problem

--- General_Problem/general.js
+++ General_Problem/general.js
@@ -10,3 +10,34 @@ function isPrime(n){
     }}
     }
     console.log(isPrime(5)) //true
+
+    // is palindrome 
+    const isPalindrome=(n)=>{
+      let copyNum=n;
+      let reverseNum=0;
+      while(copyNum>0){
+        let rem=copyNum%10;
+        reverseNum=reverseNum*10+rem;
+        copyNum=Math.floor(copyNum/10);
+        
+      }
+      return n===reverseNum
+    }
+    console.log(isPalindrome(-121))
+
+
+    // find a 6th place fibonacci number
+//1) 0 1 1 2 3 8 
+// prev curren and sum prev=0; current=1 
+// update current as current+prev and previous to current 
+const fibonacci=(n)=>{
+  let prev=0;
+  let current=1;
+  for(let i=2;i<=n;i++){
+    let temp=current;
+    current=temp+prev;
+    prev=temp
+  }
+  return current
+}
+console.log("fibonacci",fibonacci(6))
