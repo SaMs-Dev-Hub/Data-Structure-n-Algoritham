f4ea850561f3c48b62d00f00d7c8c1c711c73271
samir7171 <avhadsamir66@gmail.com>
2023-11-20 22:55:56 +0530

    added string palindrom program

--- General_Problem/general.js
+++ General_Problem/general.js
@@ -25,6 +25,18 @@ function isPrime(n){
     }
     console.log(isPalindrome(-121))
 
+    // is String palindrome
+    const isStringPalindrome=(str)=>{
+      let reverseStr="";
+      for(let i=str.length-1;i>=0;i--){
+        reverseStr+=str.charAt(i)
+      
+      }
+       if (reverseStr==str) return true;
+       return false
+      }
+      console.log(isStringPalindrome("did"))// true
+
 
     // find a 6th place fibonacci number
 //1) 0 1 1 2 3 8 
